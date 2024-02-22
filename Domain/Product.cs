57173ce58fd0f5c3748fd1ee3d16704a389332c4
Izzat Alsharif <work@izzatalsharif.com>
2024-02-22 15:01:17 +0200

    Convert `Product` price field to decimal
    Prices should be decimals for the best accuracy

--- Domain/Product.cs
+++ Domain/Product.cs
@@ -1,9 +1,9 @@
 namespace SimpleInventoryManagementSystem.Domain;
 
-public class Product(string name, int price, int quantity)
+public class Product(string name, decimal price, int quantity)
 {
     public string Name { get; set; } = name;
-    public int Price { get; set; } = price;
+    public decimal Price { get; set; } = price;
     public int Quantity { get; set; } = quantity;
 
     public override string ToString() => $"Product: {Name}, {Price} dollar(s), {Quantity} item(s)";
