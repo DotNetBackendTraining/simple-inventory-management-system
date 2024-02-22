5ff5f61bb87c09f780eab1d72dc02442a24bd500
Izzat Alsharif <work@izzatalsharif.com>
2024-02-22 14:09:16 +0200

    Override ToString() for `Product`

--- Domain/Product.cs
+++ Domain/Product.cs
@@ -5,4 +5,6 @@ public class Product(string name, int price, int quantity)
     public string Name { get; set; } = name;
     public int Price { get; set; } = price;
     public int Quantity { get; set; } = quantity;
+
+    public override string ToString() => $"Product: {Name}, {Price} dollar(s), {Quantity} item(s)";
 }
