0eec809375e5826494c17ce98ff6640f96e6274b
Izzat Alsharif <work@izzatalsharif.com>
2024-04-13 15:33:41 +0300

    Use explicit constructor

--- Domain/Product.cs
+++ Domain/Product.cs
@@ -1,10 +1,17 @@
 namespace SimpleInventoryManagementSystem.Domain;
 
-public class Product(string name, decimal price, int quantity)
+public class Product
 {
-    public string Name { get; set; } = name;
-    public decimal Price { get; set; } = price;
-    public int Quantity { get; set; } = quantity;
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+
+    public Product(string name, decimal price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
 
     public override string ToString() => $"Product: {Name}, {Price} dollar(s), {Quantity} item(s)";
 }
