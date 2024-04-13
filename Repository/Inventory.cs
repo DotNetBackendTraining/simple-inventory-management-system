e06938477b8ecade579f2e114f9fd13843f66051
Izzat Alsharif <work@izzatalsharif.com>
2024-04-13 15:36:11 +0300

    Hide products list

--- Repository/Inventory.cs
+++ Repository/Inventory.cs
@@ -4,16 +4,16 @@ namespace SimpleInventoryManagementSystem.Repository;
 
 public class Inventory
 {
-    public List<Product> Products { private get; init; } = [];
+    private readonly List<Product> _products = [];
 
-    public IEnumerable<Product> GetAllProducts() => Products;
+    public IEnumerable<Product> GetAllProducts() => _products;
 
     public Product? GetProductByName(string productName) =>
-        Products.FirstOrDefault(p => p.Name == productName);
+        _products.FirstOrDefault(p => p.Name == productName);
 
     public void DeleteProductByName(string productName) =>
-        Products.RemoveAll(p => p.Name == productName);
+        _products.RemoveAll(p => p.Name == productName);
 
     public void AddProduct(Product product) =>
-        Products.Add(product);
+        _products.Add(product);
 }
