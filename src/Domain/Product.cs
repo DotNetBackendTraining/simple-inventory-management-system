0688ad4338baeb236725573179cee0adbeecac12
Izzat Alsharif <work@izzatalsharif.com>
2024-04-14 23:15:27 +0300

    Use object initializer with required fields

--- src/Domain/Product.cs
+++ src/Domain/Product.cs
@@ -2,16 +2,10 @@ namespace SimpleInventoryManagementSystem.Domain;
 
 public class Product
 {
-    public string Name { get; set; }
-    public decimal Price { get; set; }
-    public int Quantity { get; set; }
-
-    public Product(string name, decimal price, int quantity)
-    {
-        Name = name;
-        Price = price;
-        Quantity = quantity;
-    }
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required decimal Price { get; set; }
+    public required int Quantity { get; set; }
 
     public override string ToString() => $"Product: {Name}, {Price} dollar(s), {Quantity} item(s)";
 }
